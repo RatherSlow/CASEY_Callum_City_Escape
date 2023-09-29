49f648b5a585ac1ac8ad9273b6b2e7a39ebf14b5
Callum Casey <bi25zc@student.sunderland.ac.uk>
2023-09-29 20:22:12 +0100

    Platforms, death, reset and lvl end
    Made layers and adjusted platforms, gave camera movement and added death script to (Flooris)Lava and DoomWall. Added portal to next lvl and started new input setup.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    //CharacterController characterController;
+    public float Speed;
+    public float JumpSpeed;
+    public bool isJumping;
+    //public float gravity;
+    private float Move;
+    private float Maxspeed;
+    //private Vector2 moveDirection = Vector2.zero;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        //characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Locomotion();
+        Move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(Speed * Move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, JumpSpeed));
+            Debug.Log("jump");
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
+
+    //void Locomotion()
+    //{
+    //if (characterController.isGrounded)
+    //{
+    //moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    //moveDirection = transform.TransformDirection(moveDirection);
+    //moveDirection *= Speed;
+    //}
+    //moveDirection.y -= gravity * Time.deltaTime;
+    //characterController.Move(moveDirection * Time.deltaTime);
+    //}
 }
